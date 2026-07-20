b013ff960dcc3888c3d77d359b1eea678ffc2416
unknown <onkarpawar2007@gmail.com>
2026-07-20 06:49:36 +0530

    Initial Commit, Server says hello

---
+++ backend/src/main/java/com/binary_builders/neighbourly/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.binary_builders.neighbourly.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello";
+    }
+
+    
+}
---
+++ backend/src/test/java/com/binary_builders/neighbourly/NeighbourlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binary_builders.neighbourly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighbourlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
